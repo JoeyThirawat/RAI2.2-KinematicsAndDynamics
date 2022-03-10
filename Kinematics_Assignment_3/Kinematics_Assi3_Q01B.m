262529ad2eb9ad37d8b6a6f04f0e6120f884abcc
Siriphak Amrapal <supperhappyjoey@gmail.com>
2022-03-11 00:39:13 +0700

    add assi3 q01b

--- Kinematics_Assignment_3/Kinematics_Assi3_Q01B.m
+++ Kinematics_Assignment_3/Kinematics_Assi3_Q01B.m
@@ -7,7 +7,9 @@ L1 = link([pi/2 0 th1 d1, 0]);
 L2 = link([-pi/2 a2 th2 0, 0]);
 L23 = link([pi/2 0 pi/2 0, 0]);
 L3 = link([0 0 0 d3, 1]);
+
 RRP_robot = robot({L1 L2 L23 L3});
+
 T03 = fkine (RRP_robot, [deg2rad(14.12) deg2rad(-1.43) pi/2 5])
 PO = [0;0;0;1]
 Pend = T03*PO
