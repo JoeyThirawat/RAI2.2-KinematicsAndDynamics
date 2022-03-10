aee6ebcef027bf6736c1c35fbe15db7f84dc042d
Siriphak Amrapal <supperhappyjoey@gmail.com>
2022-03-11 01:06:39 +0700

    add assi3 q05a

--- Kinematics_Assignment_3/Kinematics_Assi3_Q05A.m
+++ Kinematics_Assignment_3/Kinematics_Assi3_Q05A.m
@@ -1,5 +1,5 @@
-% Question 5
-
+%% Assingment 3 - Q05
+%  a)
 th1 = 0; th2 = 0; d3 = 1; th4 = 0;
 d1 = 5; a1 = 3; a2 = 5; d4 = 1;
 
@@ -9,6 +9,7 @@ L3 = link([0 0 0 d3, 1]);
 L4 = link([0 0 th4 d4, 0]);
 
 RRP_scara = robot ({L1 L2 L3 L4});
+
 PO = [0;0;0;1]
 T04 = fkine(RRP_scara, [deg2rad(-3.24) deg2rad(28.95) 0.5 deg2rad(0)])
 Pend = T04*PO
